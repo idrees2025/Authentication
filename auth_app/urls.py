f5b64b6682b62e8fa165801e7e531c682f74129b
idrees2025 <shahjahanidrees449@gmail.com>
2025-11-20 10:47:07 +0000

    add-email-confirmattoion

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from auth_app import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register/',views.register_view.as_view(),name='register'),
@@ -7,4 +8,9 @@ urlpatterns = [
     path('',views.dashboard_view.as_view(),name='dashboard'),
     path('resetpassword/',views.password_reset.as_view(),name='passwordreset'),
     path('logout/',views.logout__view,name='logout'),
+
+    path("passwordreset/", auth_views.PasswordResetView.as_view(), name="password_reset"),
+    path("reset/done/", auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"), 
+    path("reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path("passwordreset/done/", auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 ]
